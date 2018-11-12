4eb57a6f8f6e896791f1e3b2e85adfc807bc605b
Teresa De Jesus <teresa1594@gmail.com>
2018-11-12 22:11:37 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,12 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+require_relative './spec_helper.rb'
+
+def fizzbuzz (int).to int
+  if int % 3 == int % 5 == 0
+    "fizzbuzz"
+  elseif int % 3 == 0
+   "Fizz"
+ elseif int % 5 == 0
+  "Buzz"
+ end
