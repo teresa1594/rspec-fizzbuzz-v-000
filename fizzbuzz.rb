2bc5d12239e5831203687164b3a8f7b11ac57a55
Teresa De Jesus <teresa1594@gmail.com>
2018-11-13 00:01:15 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,7 +2,7 @@
 # See README.md for instructions on how to do this
 require_relative './spec_helper.rb'
 
-def fizzbuzz (int).to int
+def fizzbuzz (int)
   if int % 3 == 0 && int % 5 == 0
     "fizzbuzz"
   elseif int % 3 == 0
